6fc82b0b6bad93331e2f1b590a6d700a6103ad64
Akash Madavi <akashmadavi2001@gmail.com>
2025-06-21 08:22:36 +0530

    script.js

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ const apiurl = 'https://api.openweathermap.org/data/2.5/weather?&units=metric&q=
 const searchBox = document.querySelector('.search input');
 const searchBtn = document.querySelector('.search button');
 const weatherIcon = document.querySelector('.weather-icon');
+
 async function checkWeather(city) {
     const response = await fetch(apiurl + city + `&appid=${apikey}`);
     if (response.status == 404) {
         document.querySelector('.error').style.display = 'block';
         document.querySelector('.weather').style.display = 'none';
     }
+    
     var data = await response.json();
+    
     document.querySelector('.city').innerHTML = data.name;
     document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°c';
     document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
